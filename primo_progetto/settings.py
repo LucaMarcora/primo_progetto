453270f8f8d5a6e414c8f2a75d47e76183d1d9b9
LucaMarcora <luca.marcora@alessandrinimainardi.edu.it>
2024-12-04 10:44:23 +0100

    Update settings.py

--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j-+d6nruspqnfjmkwfa494&nu*rx901vb*h5%^=jro!t82hxt#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

3a7e2f6f2c4e2ef569bc5dd93058ca20bd6bdae8
LucaMarcora <luca.marcora@alessandrinimainardi.edu.it>
2025-03-05 11:49:42 +0100

    Login e Registrazione

--- news/urls.py
+++ news/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import home, articoloDetailView, listaArticoli
+from .views import home, articoloDetailView, listaArticoli, index_news
 
 app_name = 'news'
 
 urlpatterns = [
     path('',home,name='homeview'),
     path("articoli/<int:pk>", articoloDetailView, name="articolo_detail"),
-    path("lista_articoli", listaArticoli, name="lista_articoli" )
+    path("lista_articoli", listaArticoli, name="lista_articoli" ),
+    path("index_news", index_news, name="index_news" ),
 ]
